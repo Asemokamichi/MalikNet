d17a02ebdf98c9ab693c20c93143b5d17b2ec187
Asemokamichi <asemokamichi@gmail.com>
2025-05-28 19:59:19 +0500

    Initial commit

---
+++ src/main/java/com/example/maliknet/MalikNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.maliknet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalikNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalikNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maliknet/MalikNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maliknet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalikNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
